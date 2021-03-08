ac6df63138b4873cfaf59908f1d9c32797a32395
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-03-08 19:09:23 +0200

    Added private, public routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component, Suspense, lazy } from 'react';
+import { Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AppBar from './components/AppBar';
 import Layout from './components/Layout/Layout';
-import PhoneBookPage from './pages/PhoneBookPage';
-import HomePage from './pages/HomePage';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
 import authOperations from './redux/auth/auth-operations';
-import authActions from './redux/auth/auth-actions';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Spinner from './components/Spinner/Spinner';
 
-
+const HomePage = lazy(() => import('./pages/HomePage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const PhoneBookPage = lazy(() => import('./pages/PhoneBookPage'));
 
 class App extends Component {
       static propTypes = {
@@ -26,12 +27,29 @@ class App extends Component {
             <Layout>
                 <AppBar />
 
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/register" component={RegisterPage} />
-                    <Route path="/login" component={LoginPage} />
-                    <Route path="/contacts" component={PhoneBookPage} />
-                </Switch>
+                <Suspense fallback={<Spinner/>}>
+                    <Switch>
+                    <PublicRoute
+                        exact
+                        path="/"
+                        component={HomePage} />
+                    <PublicRoute
+                        path="/register"
+                        redirectTo="/contacts"
+                        restricted
+                        component={RegisterPage} />
+                    <PublicRoute
+                        path="/login"
+                        redirectTo="/contacts"
+                        restricted
+                        component={LoginPage} />
+                    <PrivateRoute
+                        path="/contacts"
+                        component={PhoneBookPage}
+                        redirectTo="/login"
+                    />
+                    </Switch>
+                 </Suspense>
             </Layout>
         );
     };
